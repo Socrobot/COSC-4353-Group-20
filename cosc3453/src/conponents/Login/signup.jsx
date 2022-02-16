52d1e172ffcb09b39a34ec30b917d9fd05b6b8be
Wesley Koons <socrobot@gmail.com>
2022-02-16 15:42:16 -0600

    Signin/signup frontend.

---
+++ cosc3453/src/conponents/Login/signup.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { BC, FC, I, ML, SB, BL } from "./common";
+import { Marginer } from "../marginerTool";
+import { AccountContext } from "./accountContext";
+
+export function Signup(props) {
+
+    const { toSignin } = useContext(AccountContext)
+
+    return <BC>
+        <FC>
+            <I type="text" placeholder="Full Name" />
+            <I type="text" placeholder="Company Name" />
+            <I type="email" placeholder="Email" />
+            <I type="password" placeholder="Password" />
+            <I type="password" placeholder="Confirm Password" />
+        </FC>
+        <Marginer direction="vertical" margin="1.6em" />
+        <SB type="submit">Signin</SB>
+        <ML href="#">
+            Already Have An Account? <BL href="#" onClick={toSignin}>Signin</BL>
+        </ML>
+    </BC>
+}
