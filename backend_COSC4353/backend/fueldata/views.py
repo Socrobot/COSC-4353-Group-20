10380524a7f8e7f319da667eacd92c650871c0c5
mvsen <masen80016@gmail.com>
2022-03-13 18:22:58 -0500

    new

--- backend_COSC4353/backend/fueldata/views.py
+++ backend_COSC4353/backend/fueldata/views.py
@@ -19,12 +19,3 @@ class FuelDataView(viewsets.ModelViewSet):
     serializer_class = FuelDataSerializer
     queryset = FuelData.objects.all()
 
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid(raise_exception=False):
-            return Response({"Fail": "Used or Invalid Email"}, status=status.HTTP_400_BAD_REQUEST)
-
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response({"Success": "Accepted Email"}, status=status.HTTP_201_CREATED, headers=headers)
