f58e145a7e116ce90fa1375add471cc154d8a2f8
shazmaknojia <shazmaknojia12@gmail.com>
2022-03-31 19:05:04 -0500

    updated user stuff

--- backend_COSC4353/backend/userdata/serializers.py
+++ backend_COSC4353/backend/userdata/serializers.py
@@ -4,12 +4,12 @@ from .models import UserInfo
 
 class UserInfoSerializer(serializers.ModelSerializer):
     
-    Namefield = serializers.CharField(max_length=50)
-    Addressfield = serializers.CharField(max_length=100)
-    Address2field = serializers.CharField(max_length=100)
-    Cityfield = serializers.CharField(max_length=100)
-    Statefield = serializers.CharField(max_length=2)
-    ZipCodefield = serializers.CharField(max_length=9)
+    # Namefield = serializers.CharField(max_length=50)
+    # Addressfield = serializers.CharField(max_length=100)
+    # Address2field = serializers.CharField(max_length=100)
+    # Cityfield = serializers.CharField(max_length=100)
+    # Statefield = serializers.CharField(max_length=2)
+    # ZipCodefield = serializers.CharField(max_length=9)
     
     
     class Meta:
