eb838ccbb3d49a4f5ae501163e17b67ed3a94aac
Wesley Koons <socrobot@gmail.com>
2022-04-02 22:29:08 -0500

    Assignment 4 final push

--- backend_COSC4353/backend/userdata/models.py
+++ backend_COSC4353/backend/userdata/models.py
@@ -1,15 +1,19 @@
 from email.headerregistry import Address
 from django.db import models
-import string, random
+import string
+import random
 
 # Create your models here.
+
+
 class UserInfo(models.Model):
-    Namefield = models.CharField(max_length=50,unique =True)
-    Addressfield = models.CharField(max_length=100,unique =True)
-    Address2field = models.CharField(max_length=100, default="",blank=True)
+    username = models.CharField(max_length=100, default='invaliddata')
+    Namefield = models.CharField(max_length=50, unique=True)
+    Addressfield = models.CharField(max_length=100, unique=True)
+    Address2field = models.CharField(max_length=100, blank=True)
     Cityfield = models.CharField(max_length=100)
     Statefield = models.CharField(max_length=2)
-    ZipCodefield = models.CharField(max_length=9) 
+    ZipCodefield = models.CharField(max_length=9)
 
     def __unicode__(self):
         return self.Namefield
--- backend_COSC4353/backend/userdata/serializers.py
+++ backend_COSC4353/backend/userdata/serializers.py
@@ -4,15 +4,14 @@ from .models import UserInfo
 
 class UserInfoSerializer(serializers.ModelSerializer):
     
-    Namefield = serializers.CharField(max_length=50)
-    Addressfield = serializers.CharField(max_length=100)
-    Address2field = serializers.CharField(max_length=100)
-    Cityfield = serializers.CharField(max_length=100)
-    Statefield = serializers.CharField(max_length=2)
-    ZipCodefield = serializers.CharField(max_length=9)
+    # Namefield = serializers.CharField(max_length=50)
+    # Addressfield = serializers.CharField(max_length=100)
+    # Address2field = serializers.CharField(max_length=100)
+    # Cityfield = serializers.CharField(max_length=100)
+    # Statefield = serializers.CharField(max_length=2)
+    # ZipCodefield = serializers.CharField(max_length=9)
     
     
     class Meta:
         model = UserInfo
         fields = ('Namefield','Addressfield','Address2field','Cityfield','Statefield','ZipCodefield')
-
